e76e20d9860d09df50c2b1019393a3b846c8dc8d
Yoh Tanimoto <hoyt@jcom.home.ne.jp>
2021-02-07 15:33:11 +0100

    update data

--- download-iss.py
+++ download-iss.py
@@ -23,8 +23,8 @@ for line in lines:
   fields = line.split(',')
   writing = False
   for word in fields:
-   if '\"y\":[18' in word:
-    dailyy.append("18")
+   if '\"y\":[19' in word:
+    dailyy.append("19")
     writing = True
     continue
    if writing == True and ']' in word:
@@ -48,8 +48,8 @@ for line in lines:
   fields = line.split(',')
   writing = False
   for word in fields:
-   if '\"y\":[16' in word:
-    dailyy2.append("16")
+   if '\"y\":[17' in word:
+    dailyy2.append("17")
     writing = True
     continue
    if writing == True and ']' in word:
